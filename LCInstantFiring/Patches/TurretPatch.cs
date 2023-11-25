c27b142f569e5ecfc528af2e4e99cd959de66e14
Bagiwka <sagivka2006@gmail.com>
2023-11-25 20:59:59 +0100

    init

---
+++ LCInstantFiring/Patches/TurretPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+[HarmonyPatch(typeof(Turret))]
+[HarmonyPatch("Update")]
+class TurretPatch
+{
+    static void Postfix(Turret __instance, ref float _turretInterval)
+    {
+        if (__instance.turretActive && __instance.turretMode == TurretMode.Detection)
+        {
+            if (_turretInterval >= 1.5f)
+            {
+                SetTurretMode(__instance, TurretMode.Firing);
+            }
+        }
+    }
+
+    static void SetTurretMode(Turret turret, TurretMode mode)
+    {
+        turret.turretMode = mode;
+        turret.SetToModeClientRpc((int)mode);
+    }
+}
---
+++ LCInstantFiring/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LCInstantTurrets
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LCInstantTurretsBase : BaseUnityPlugin
+    {
+
+        private const string modGUID = "Bagiwka.LCInstantTurrets";
+        private const string modName = "LCInstantTurrets";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LCInstantTurretsBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+
+            Logger.LogInfo("Time to get shot.");
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Get your legs ready!");
+
+            harmony.PatchAll(typeof(LCInstantTurretsBase));
+            harmony.PatchAll(typeof(TurretPatch));
+
+
+        }
+
+    }
+}
